16e3b9fe332076f4e04ba06ba0daf172e56c41ee
Emory Strother <estrother2014@kellogg.northwestern.edu>
2013-10-29 16:53:44 -0500

    Initial SetUp
    First folders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Githelp::Application.config.secret_key_base = 'ff8ba4eb0d19793143353f2988c07f592b7cab095dd9c7f452a0785a077c4ebf8749b56bcc5cb6cdf1039e9ab6ac5b3ad4d9328ea5a39268d97370f54e111ef6'
